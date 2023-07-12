7760c0ed696978f15b061bf0017d7811e461f2c7
VedantY17 <vedant.ny@gmail.com>
2023-07-12 15:39:15 +0530

    first commit

---
+++ public/main.js
@@ -0,0 +1,92 @@
+const socket = io()
+
+const clientsTotal = document.getElementById('client-total')
+
+const messageContainer = document.getElementById('message-container')
+const nameInput = document.getElementById('name-input')
+const messageForm = document.getElementById('message-form')
+const messageInput = document.getElementById('message-input')
+
+const messageTone = new Audio('/tone.mp3')
+
+messageForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  sendMessage()
+})
+
+socket.on('clients-total', (data) => {
+  clientsTotal.innerText = `Total Clients: ${data}`
+})
+
+function sendMessage() {
+  if (messageInput.value === '') return
+  // console.log(messageInput.value)
+  const data = {
+    name: nameInput.value,
+    message: messageInput.value,
+    dateTime: new Date(),
+  }
+  socket.emit('message', data)
+  addMessageToUI(true, data)
+  messageInput.value = ''
+}
+
+socket.on('chat-message', (data) => {
+  // console.log(data)
+  messageTone.play()
+  addMessageToUI(false, data)
+})
+
+function addMessageToUI(isOwnMessage, data) {
+  clearFeedback()
+  const dateTime = new Date(); // Get the current date and time
+  const formattedDateTime = moment(dateTime).format('DD-MM-YYYY HH:mm'); // Format the dateTime
+  const element = `
+      <li class="${isOwnMessage ? 'message-right' : 'message-left'}">
+          <p class="message">
+            ${data.message}
+            <span>${data.name} ● ${formattedDateTime}</span>
+          </p>
+        </li>
+        `
+        
+  messageContainer.innerHTML += element
+  scrollToBottom()
+}
+
+function scrollToBottom() {
+  messageContainer.scrollTo(0, messageContainer.scrollHeight)
+}
+
+messageInput.addEventListener('focus', (e) => {
+  socket.emit('feedback', {
+    feedback: `✍️ ${nameInput.value} is typing a message`,
+  })
+})
+
+messageInput.addEventListener('keypress', (e) => {
+  socket.emit('feedback', {
+    feedback: `✍️ ${nameInput.value} is typing a message`,
+  })
+})
+messageInput.addEventListener('blur', (e) => {
+  socket.emit('feedback', {
+    feedback: '',
+  })
+})
+
+socket.on('feedback', (data) => {
+  clearFeedback()
+  const element = `
+        <li class="message-feedback">
+          <p class="feedback" id="feedback">${data.feedback}</p>
+        </li>
+  `
+  messageContainer.innerHTML += element
+})
+
+function clearFeedback() {
+  document.querySelectorAll('li.message-feedback').forEach((element) => {
+    element.parentNode.removeChild(element)
+  })
+}
